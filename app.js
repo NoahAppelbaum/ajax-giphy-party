59e275af2a52ea80f3450b4b22b60c2339f2225f
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-15 18:37:20 -0500

    made basic html outline and started writing api call function

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Let's get this party started!");
+const API_KEY = "a4spUafLfMvXRhFWpwTlHP6sGHpLQJwr";
+
+
+function handleSearch() {
+  const searchTerm = $(input).val();
+
+
+
+}
+
+
+async function getGif(searchTerm) {
+
+  const parameters = new URLSearchParams({
+    api_key: API_KEY,
+    q: searchTerm, limit: 1
+  });
+  const response = await fetch(`api.giphy.com/v1/gifs/search?${parameters}`);
+  const embedURL = await response.json().data.embed_url;
+
+  //TEST
+  return embedURL;
+
+}
+
+
+//json object
+//.data
+//.embed_url
