e5763f4beb4dbb132a432ec43fd17d4f9f0ebc60
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-15 19:15:49 -0500

    api works

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
+"use strict";
+
 console.log("Let's get this party started!");
 const API_KEY = "a4spUafLfMvXRhFWpwTlHP6sGHpLQJwr";
 
 
-function handleSearch() {
-  const searchTerm = $(input).val();
+async function handleSearch(evt) {
+  evt.preventDefault();
 
+  const searchTerm = $("input").val();
 
+  const embedUrl = await getGifUrl(searchTerm);
 
+  addImageToPage(embedUrl);
+
+  // $("form").get().reset();
 }
 
-
-async function getGif(searchTerm) {
-
+async function getGifUrl(searchTerm) {
   const parameters = new URLSearchParams({
     api_key: API_KEY,
-    q: searchTerm, limit: 1
+    q: searchTerm,
+    limit: 1
   });
-  const response = await fetch(`api.giphy.com/v1/gifs/search?${parameters}`);
-  const embedURL = await response.json().data.embed_url;
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search?${parameters}`);
+  const jsonResponse = await response.json();
+  const embedUrl = await jsonResponse.data[0].embed_url;
 
-  //TEST
-  return embedURL;
+  return embedUrl;
+}
+
+
+function addImageToPage(embedUrl) {
+  const $img = $("<img>").attr("src", embedUrl)
+debugger;
+  $(".display-area").append($img.get())
 
 }
 
 
-//json object
-//.data
-//.embed_url
+$("form").on("submit", handleSearch)
