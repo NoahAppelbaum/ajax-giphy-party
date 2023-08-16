c386cb832ca73489e15254d34e3c0a5e8e036b90
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-15 19:29:36 -0500

    working app. Needs styling.

--- app.js
+++ app.js
@@ -3,19 +3,22 @@
 console.log("Let's get this party started!");
 const API_KEY = "a4spUafLfMvXRhFWpwTlHP6sGHpLQJwr";
 
-
+/** handleSearch: gets search term from form and calls functions
+ * to search with API and post image to page
+ */
 async function handleSearch(evt) {
   evt.preventDefault();
 
   const searchTerm = $("input").val();
 
-  const embedUrl = await getGifUrl(searchTerm);
+  const gifUrl = await getGifUrl(searchTerm);
 
-  addImageToPage(embedUrl);
+  addImageToPage(gifUrl);
 
   // $("form").get().reset();
 }
 
+/** getGifUrl: accepts string searchTerm to make API call to GIPHY for a URL */
 async function getGifUrl(searchTerm) {
   const parameters = new URLSearchParams({
     api_key: API_KEY,
@@ -24,18 +27,24 @@ async function getGifUrl(searchTerm) {
   });
   const response = await fetch(`https://api.giphy.com/v1/gifs/search?${parameters}`);
   const jsonResponse = await response.json();
-  const embedUrl = await jsonResponse.data[0].embed_url;
+  const gifUrl = await jsonResponse.data[0].images.original.url;
 
-  return embedUrl;
+  return gifUrl;
+}
+
+/** addImageToPage: Updates DOM with new GIF based on passed URL in the display area */
+function addImageToPage(gifUrl) {
+  const $img = $("<img>").attr("src", gifUrl);
+  $(".display-area").append($img.get());
+
 }
 
 
-function addImageToPage(embedUrl) {
-  const $img = $("<img>").attr("src", embedUrl)
-debugger;
-  $(".display-area").append($img.get())
-
+/** removeGifs: clears display area in DOM */
+function removeGifs() {
+  $(".display-area").html("");
 }
 
 
-$("form").on("submit", handleSearch)
+$("form").on("submit", handleSearch);
+$(".remove-button").on("click", removeGifs);
